b76fc5c6be2e4ffb414c538cb7a2db288064124c
James Anderson <james.anderson@wright.edu>
2025-09-10 14:46:04 -0400

    have text formatted pretty good

--- androidExamples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ androidExamples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -14,12 +14,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -30,39 +37,63 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingText(
+                        message = "Happy Birthday Paris!",
+                        from = "From James",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
 
+                    )
                 }
             }
         }
     }
 }
 
+/**
+ * GreetingText shows the message and from strings in a Column
+ */
 @Composable
 fun GreetingText(
     message: String,
     from: String,
     modifier: Modifier = Modifier
 ) {
-    // Happy Birthday to...
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly, // you can play with different arrangements
+        modifier = modifier
+            .padding(8.dp)
+    ) {
+        // Happy Birthday ...
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
 
-    // from...
-    Text(
-        text = from,
-        fontSize = 36.sp,
-    )
 
+        // from ...
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Right, // this only aligns the text to the right within its box
+            modifier = Modifier
+                .padding(8.dp)
+                .align(alignment = Alignment.End) // this aligns the entire text composable to the right
+        )
+    }
 }
 
 @Preview(showBackground = false)
 @Composable
 fun BirthdayCardPreview() {
-    GreetingText(
-        message = "Happy Birthday James!",
-        from = "From Paris"
-    )
+    HappyBirthdayTheme {
+        GreetingText(
+            message = "Happy Birthday James!",
+            from = "From Paris",
+            modifier = Modifier.fillMaxSize() // unless I pass fillMaxSize(), the app only takes up the minimum amount of space required
+        )
+    }
 }
