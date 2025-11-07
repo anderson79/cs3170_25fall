12db3e6ace92c7cb3e1912ab88987b3352c77ecc
James Anderson <james.anderson@wright.edu>
2025-11-07 13:35:05 -0500

    added import for project 3 to make it work

--- projects/Project3_CharacterCreatorRemastered/app/src/main/java/com/example/charactercreator/AttributeList.kt
+++ projects/Project3_CharacterCreatorRemastered/app/src/main/java/com/example/charactercreator/AttributeList.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.charactercreator.data.DataSource.attribList
 
 @Composable
 fun AttributeList(
